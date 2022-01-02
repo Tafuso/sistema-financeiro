d92783a9cf88029c91abe9c3b603f895c20bac0d
Tafuso <gustavovasquez2002@gmail.com>
2022-01-02 17:05:08 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 1, 6),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2022, 1, 15),
+    category: 'food',
+    title: 'Burguer King',
+    value: 28
+  },
+  {
+    date: new Date(2022, 2, 16),
+    category: 'rent',
+    title: 'Aluguel',
+    value: 2300
+  },
+  {
+    date: new Date(2022, 1, 15),
+    category: 'salary',
+    title: 'Salário',
+    value: 4500
+  }
+]
